167eaf475be1f42f0f70712e4c72286e4c272fb1
Md. Ehtisham Farooqui <mefxe01@gmail.com>
2021-02-11 23:11:46 +0530

    styled the homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import CTA from '../styled/CTA'
+import StyledTitle from '../styled/StyledTitle'
+import {Accent} from '../styled/Accent'
+
 
 export default function Home(){
   return(
     <div>
-      <h1>Home</h1>
+      <StyledTitle>Ready to Type?</StyledTitle>
+      <CTA to="/game">Click or Press <Accent>'s'</Accent> to start playing!</CTA>
     </div>
   )
 }
---
+++ src/styled/CTA.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+import {Link} from 'react-router-dom'
+
+export default styled(Link)`
+  font-size: 1.2rem;
+  text-align: center;
+  display: block;
+  text-decoration: none;
+  &:hover{
+    text-decoration: underline;
+    
+  }
+`
